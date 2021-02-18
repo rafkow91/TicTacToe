2e905d730b6976bb26e933c2c0ead9c12a22eedd
rafkow91 <64289498+rafkow91@users.noreply.github.com>
2021-02-18 16:10:08 +0100

    Definition function: displayBoard

--- Kolko_i_krzyzyk.py
+++ Kolko_i_krzyzyk.py
@@ -6,7 +6,7 @@ def displayBoard(board):
     for i in range (3):
         print("-"*13+"\n| "+ str(board[i * 3])+" | " + str(board[i * 3 + 1])+" | " + str(board[i * 3 + 2])+" | ")
         i = i + 1
-    print("-"*13+"\n| ")
+    print("-"*13)
 
 tablica = [1,2,3,4,5,6,7,8,9]
 displayBoard(tablica)
